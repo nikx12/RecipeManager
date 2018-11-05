3b4d8f33a67fe6369295f575c4c2f7ae42b2c585
nikx12 <nikhitazulka@gmail.com>
2018-11-05 14:52:10 +0530

    Updated the mongose info in index file

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 mongoose.set('debug', true);
 mongoose.connect('mongodb://localhost:27017/RecipeManager');
-mongoose.Promise = Promise;
+mongoose.Promise = global.Promise;
 
 module.exports.User = require('./user');
