fba34247b9fac21fedc95f6002f51864e542e2db
nikx12 <nikhitazulka@gmail.com>
2018-11-08 23:56:19 +0530

    Updated the schema for recipe

--- models/recipe.js
+++ models/recipe.js
@@ -1,11 +1,17 @@
- var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/RecipeManager', { useNewUrlParser: true });
+var mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb://localhost:27017/RecipeManager",
+  { useNewUrlParser: true }
+);
 var recipeSchema = new mongoose.Schema({
-   userName: String,
-   title: String,
-   image: String,
-   description: String,
-   });
+  userName: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+  title: String,
+  image: String,
+  description: String
+});
 
 var Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;
